870febb588a303df6a371d642d7bcccc4d08a1ae
Tselane2 <huaweikcg@gmail.com>
2026-05-29 11:51:30 +0200

    Delete lib directory

--- lib/db/dist/schema/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./users";
-export * from "./publishers";
-export * from "./articles";
-export * from "./newsletters";
-export * from "./subscriptions";
-//# sourceMappingURL=index.d.ts.map
